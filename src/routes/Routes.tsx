84157b5bfc9c888b714fd5aa089a68d49ea86a5e
[디플루이드] 노성호 <nightwing@naver.com>
2025-05-24 10:58:33 +0900

    asdfasdf

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,12 +3,31 @@ import { Routes, Route } from 'react-router-dom';
 import MainMenu from '../scenes/MainMenu';
 import CharacterSelect from '../scenes/CharacterSelect';
 import UiTest from '../scenes/UiTest';
+import {
+  FirstHalf01,
+  FirstHalf02,
+  FirstHalf03,
+  FirstHalf04,
+  FirstHalf05,
+  FirstHalf06,
+  FirstHalf07,
+  FirstHalf08,
+} from '../scenes/rooms/index';
+
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainMenu />} />
       <Route path="/character-select" element={<CharacterSelect />} />
       <Route path="/ui-test" element={<UiTest />} />
+      <Route path="/first-half-01" element={<FirstHalf01 />} />
+      <Route path="/first-half-02" element={<FirstHalf02 />} />
+      <Route path="/first-half-03" element={<FirstHalf03 />} />
+      <Route path="/first-half-04" element={<FirstHalf04 />} />
+      <Route path="/first-half-05" element={<FirstHalf05 />} />
+      <Route path="/first-half-06" element={<FirstHalf06 />} />
+      <Route path="/first-half-07" element={<FirstHalf07 />} />
+      <Route path="/first-half-08" element={<FirstHalf08 />} />
       {/* 필요에 따라 다른 라우트들을 여기에 추가합니다. */}
       {/* 예: <Route path="/gameplay" element={<GamePlayScene />} /> */}
       {/* 예: <Route path="*" element={<NotFoundPage />} /> */}
---
+++ src/scenes/rooms/index.ts
@@ -0,0 +1,8 @@
+export { default as FirstHalf01 } from './FirstHalf01';
+export { default as FirstHalf02 } from './FirstHalf02';
+export { default as FirstHalf03 } from './FirstHalf03';
+export { default as FirstHalf04 } from './FirstHalf04';
+export { default as FirstHalf05 } from './FirstHalf05';
+export { default as FirstHalf06 } from './FirstHalf06';
+export { default as FirstHalf07 } from './FirstHalf07';
+export { default as FirstHalf08 } from './FirstHalf08';
